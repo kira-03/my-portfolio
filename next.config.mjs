4b12765766a980eb0d665a0ae5f7f482b1664606
Guru <guruprasathmaheswaran.07@gmail.com>
2024-07-07 22:42:05 +0530

    DONE

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
