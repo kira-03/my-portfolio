2a5141b4f7a3b621b819c9ccf0de83c7f96657c2
Guru <guruprasathmaheswaran.07@gmail.com>
2024-07-17 19:29:03 +0530

    microsoft clarity

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ const Clarity = () => {
       dangerouslySetInnerHTML={{
         __html: `
           (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "n8su3wehf9");
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "n8su3wehf9");
         `,
       }}
     />
