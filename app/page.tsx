52e4b585edadf4d7964e461d52d32e39533abd3c
Guru <guruprasathmaheswaran.07@gmail.com>
2024-07-17 19:27:57 +0530

    microsoft clarity

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,27 @@ import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
+const Clarity = () => {
+  return (
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "n8su3wehf9");
+        `,
+      }}
+    />
+  );
+};
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <Clarity />
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
