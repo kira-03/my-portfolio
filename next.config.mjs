4f46955f5ba23549134377fe51e4512f6c17900e
Guru <guruprasathmaheswaran.07@gmail.com>
2025-06-26 17:28:29 +0530

    reverted back

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    images: { unoptimized: true },
-    basePath: '/guruprasath-portfolio',
-    assetPrefix: '/guruprasath-portfolio/',
+    images: { unoptimized: true }
 };
 
 export default nextConfig;
