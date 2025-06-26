d95a2d42182e7716944cc8e20229267f198242e4
Guru <guruprasathmaheswaran.07@gmail.com>
2025-06-26 17:06:56 +0530

    gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    images: { unoptimized: true },
+    basePath: '/guruprasath-portfolio',
+    assetPrefix: '/guruprasath-portfolio/',
 };
+
+export default nextConfig;
 
